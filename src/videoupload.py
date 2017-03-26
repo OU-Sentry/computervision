356ad6227238b6cafe51da6c8b104533f7c2b70a
Jacob Pleiness <jacobpleiness@Mentat.local>
2017-03-26 18:07:21 -0400

    rough version of file watching and uploading working

--- src/videoupload.py
+++ src/videoupload.py
@@ -1,12 +1,19 @@
 import time
+import tinys3
+from os import environ
 from os import listdir
 from os import path
 
 
 class Watcher():
-    def __init__(self, watchdir="."):
+    def __init__(self, watchdir=".", delta=60):
         self.watchdir = watchdir
         self.files = {}
+        self.delta = delta
+        self.pool = tinys3.Pool(environ.get('S3_ACCESS_KEY'),
+                                environ.get('S3_SECRET_KEY'),
+                                tls=True,
+                                default_bucket='sentryvideostorage')
 
     def scan(self):
         # scan over files in watchdir, update self.files accordingly
@@ -18,7 +25,14 @@ class Watcher():
         # scan over dict, compare time last modified with current time
         # if delta is over n minutes, list of files to upload to S3
         for key in self.files:
-            print(self.files[key])
+            if (time.time() - self.files[key]) >= self.delta:
+                print("here is one older than a minute ", key)
+                f = open(key, 'rb')
+                r = self.pool.upload(key, f)
+                while r.done() != True:
+                    time.sleep(5)
+                    print(r)
+                    print("waiting on upload")
 
 
 if __name__ == '__main__':
